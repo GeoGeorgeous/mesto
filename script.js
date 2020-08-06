36500c85e67dd1149f98bf9479c73cd15c8d5fe6
George Gorchev <georgegorchev@yandex.ru>
2020-08-06 03:59:24 +0300

    feat: add ability to change name and description via popup

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
+let saveButton = document.querySelector('.popup__container');
 let accountName = document.querySelector('#name');
 let accountDescription = document.querySelector('#description');
-
-
-let inputName = document.querySelector('#input_name');
-let inputDescription = document.querySelector('#input_description');
-console.log(inputName);
-
-// accountName.textContent = "Жак-Ив Кусто";
-// accountDescription.textContent = "Исследователь океана";
 
 function showPopUp() {
   let popUp = document.querySelector('.popup');
@@ -22,11 +15,21 @@ function hidePopUp() {
 }
 
 function getDefaultInput() {
+  let inputName = document.querySelector('#input_name');
+  let inputDescription = document.querySelector('#input_description');
   inputName.value = accountName.textContent;
   inputDescription.value = accountDescription.textContent;
 }
 
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  let inputName = document.querySelector('#input_name');
+  let inputDescription = document.querySelector('#input_description');
+  accountName.textContent = inputName.value;
+  accountDescription.textContent = inputDescription.value;
+  hidePopUp();
+}
 getDefaultInput();
 editButton.addEventListener('click', showPopUp);
 closeButton.addEventListener('click', hidePopUp);
-
+saveButton.addEventListener('submit', formSubmitHandler);
