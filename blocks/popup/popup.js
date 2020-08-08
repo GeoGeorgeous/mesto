418c661dd5cf9fd60c6c408889885ed21245846a
George Gorchev <georgegorchev@yandex.ru>
2020-08-08 03:37:39 +0300

    fix: redo js

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,35 +1,38 @@
+// Popup window
+let popUp = document.querySelector('.popup');
+// Edit button
 let editButton = document.querySelector('.profile__edit-button');
+// Close button
 let closeButton = document.querySelector('.popup__close-button');
+// Save button
 let saveButton = document.querySelector('.popup__container');
-let accountName = document.querySelector('#name');
-let accountDescription = document.querySelector('#description');
+// Account name
+let accountName = document.querySelector('.profile__name');
+// Account Description
+let accountDescription = document.querySelector('.profile__description');
+// Input field for name
+let input_accountName = document.querySelector('.popup__form-item_input_name');
+// Input field for desc
+let input_accountDescription = document.querySelector('.popup__form-item_input_description');
 
 function showPopUp() {
-  let popUp = document.querySelector('.popup');
+  // get current account name and description and put in.
+  input_accountName.value = accountName.textContent;
+  input_accountDescription.value = accountDescription.textContent;
+  // show popup
   popUp.classList.add('popup_opened');
 }
 
 function hidePopUp() {
-  let popUp = document.querySelector('.popup');
   popUp.classList.remove('popup_opened');
-}
-
-function getDefaultInput() {
-  let inputName = document.querySelector('#input_name');
-  let inputDescription = document.querySelector('#input_description');
-  inputName.value = accountName.textContent;
-  inputDescription.value = accountDescription.textContent;
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-  let inputName = document.querySelector('#input_name');
-  let inputDescription = document.querySelector('#input_description');
-  accountName.textContent = inputName.value;
-  accountDescription.textContent = inputDescription.value;
-  hidePopUp();
+  accountName.textContent = input_accountName.value;
+  accountDescription.textContent = input_accountDescription.value;
 }
-getDefaultInput();
+
 editButton.addEventListener('click', showPopUp);
 closeButton.addEventListener('click', hidePopUp);
 saveButton.addEventListener('submit', formSubmitHandler);
