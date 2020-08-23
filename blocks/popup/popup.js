fa5b808faac48565766503dc7cf1f8083f432ce3
George Gorchev <georgegorchev@yandex.ru>
2020-08-23 16:26:02 +0300

    feat: add place popup

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,39 +1,66 @@
-// Popups
-const popUp = document.querySelector('.popup.popup_target_edit-account');
-// Edit button
-const editButton = document.querySelector('.profile__edit-button');
-// Close button
-const closeButton = popUp.querySelector('.popup__close-button');
-// Save button
-const saveButton = popUp.querySelector('.popup__container');
-// Account name
-const accountName = document.querySelector('.profile__name');
-// Account Description
-const accountDescription = document.querySelector('.profile__description');
-// Input field for name
-const inputAccountName = popUp.querySelector('.popup__form-item_input_name');
-// Input field for desc
-const inputAccountDescription = popUp.querySelector('.popup__form-item_input_description');
+const accountPopUp = document.querySelector('.popup[data-type="account"]'); // Edit Account popUp
+const accountEditButton = document.querySelector('.profile__edit-button'); // Edit Account button
+const accountCloseButton = accountPopUp.querySelector('.popup__close-button'); // Close Account popUp button
+const accountSaveButton = accountPopUp.querySelector('.popup__container'); // Save Account popUp button
+const accountInputName = accountPopUp.querySelector('.popup__form-item_input_name'); // Input field in Account popUp
+const accountInputDesc = accountPopUp.querySelector('.popup__form-item_input_description'); // // Input field in Account popUp
 
-function popUpToggle() {
-  popUp.classList.toggle('popup_opened')
+const accountName = document.querySelector('.profile__name'); // Account name
+const accountDescription = document.querySelector('.profile__description'); // Account Description
+
+const placePopUp = document.querySelector('.popup[data-type="place"]'); // Add Place popUp
+const placeAddButton = document.querySelector('.profile__add-button'); // Add Place button
+const placeCloseButton = placePopUp.querySelector('.popup__close-button'); // Close Add Place popUp button
+const placeSaveButton = placePopUp.querySelector('.popup__container'); // Save Add Place popUp button
+const placeInputTitle = placePopUp.querySelector('.popup__form-item_input_name'); // Input field for title
+const placeInputLink = placePopUp.querySelector('.popup__form-item_input_description'); // // Input field for link
+
+
+function popUpToggle(obj) {
+  obj.classList.toggle('popup_opened')
 }
 
-function showPopUp() {
+function showAccountPopUp() {
   // get current account name and description and put them into input
-  inputAccountName.value = accountName.textContent;
-  inputAccountDescription.value = accountDescription.textContent;
-  // show popup
-  popUpToggle();
+  accountInputName.value = accountName.textContent;
+  accountInputDesc.value = accountDescription.textContent;
+  // toggle (show) popup
+  popUpToggle(accountPopUp);
 }
 
-function formSubmitHandler (evt) {
+function closeAccountPopUp() {
+  popUpToggle(accountPopUp);
+}
+
+function accountFormSubmitHandler (evt) {
   evt.preventDefault();
-  accountName.textContent = inputAccountName.value;
-  accountDescription.textContent = inputAccountDescription.value;
-  popUpToggle();
+  accountName.textContent = accountInputName.value;
+  accountDescription.textContent = accountInputDesc.value;
+  popUpToggle(accountPopUp);
 }
 
-editButton.addEventListener('click', showPopUp);
-closeButton.addEventListener('click', popUpToggle);
-saveButton.addEventListener('submit', formSubmitHandler);
+accountEditButton.addEventListener('click', showAccountPopUp);
+accountCloseButton.addEventListener('click', closeAccountPopUp);
+accountSaveButton.addEventListener('submit', accountFormSubmitHandler);
+
+
+// placePopUp
+
+
+function showPlacePopUp() {
+  popUpToggle(placePopUp);
+}
+
+function closePlacePopUp() {
+  popUpToggle(placePopUp);
+}
+
+function placeFormSubmitHandler (evt) {
+  evt.preventDefault();
+  popUpToggle(placePopUp);
+}
+
+placeAddButton.addEventListener('click', showPlacePopUp);
+placeCloseButton.addEventListener('click', closePlacePopUp);
+placeSaveButton.addEventListener('submit', placeFormSubmitHandler);
+
