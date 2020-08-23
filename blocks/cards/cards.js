a7f903919e562402d479e08b3b18d675c756b160
George Gorchev <georgegorchev@yandex.ru>
2020-08-23 14:41:20 +0300

    feat: Init cards from the box

---
+++ blocks/cards/cards.js
@@ -0,0 +1,51 @@
+// array with names / links for default cards
+const initialCards = [
+  {
+      name: 'Покровка',
+      link: './images/card__Pokrovka.jpg'
+  },
+  {
+      name: 'ЦУМ',
+      link: './images/card__TeatralnyjProezd.jpg'
+  },
+  {
+      name: 'Москва-сити',
+      link: './images/card__MoscowCity.jpg'
+  },
+  {
+      name: 'Мясницкая',
+      link: './images/card__Myasnickaya.jpg'
+  },
+  {
+      name: 'Чертаново',
+      link: './images/card__Chertanovo.jpg'
+  },
+  {
+      name: 'метро Добрынинская',
+      link: './images/card__Dobryninskaya.jpg'
+  }
+];
+
+// get the list of all the cards on the page
+const cardsContainer = document.querySelector('.cards__items');
+// get the card template
+const cardTemplate = document.querySelector('#card').content;
+
+
+// this function gets an array (arr) and creates a new card using cardTemplate
+function initCards(arr) {
+  arr.forEach(element => {
+    // clone the template content
+    const card = cardTemplate.cloneNode(true);
+    // set card title,image and image alt
+    card.querySelector('.card__title').textContent = element.name;
+    card.querySelector('.card__image').src = element.link;
+    card.querySelector('.card__image').alt = element.name;
+    // add this new card to cardsContainer and move to next element
+    cardsContainer.append(card);
+  });
+}
+
+// initial default cards
+initCards(initialCards);
+
