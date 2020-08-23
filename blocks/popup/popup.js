c34036d444c6d9498cf4c6bca40a0ac87a1572e0
George Gorchev <georgegorchev@yandex.ru>
2020-08-23 15:12:35 +0300

    fix: change let to const and add/remove to toggle popup class

--- blocks/popup/popup.js
+++ blocks/popup/popup.js
@@ -1,39 +1,39 @@
-// Popup window
-let popUp = document.querySelector('.popup');
+// Popups
+const popUp = document.querySelector('.popup.popup_target_edit-account');
 // Edit button
-let editButton = document.querySelector('.profile__edit-button');
+const editButton = document.querySelector('.profile__edit-button');
 // Close button
-let closeButton = document.querySelector('.popup__close-button');
+const closeButton = popUp.querySelector('.popup__close-button');
 // Save button
-let saveButton = document.querySelector('.popup__container');
+const saveButton = popUp.querySelector('.popup__container');
 // Account name
-let accountName = document.querySelector('.profile__name');
+const accountName = document.querySelector('.profile__name');
 // Account Description
-let accountDescription = document.querySelector('.profile__description');
+const accountDescription = document.querySelector('.profile__description');
 // Input field for name
-let inputAccountName = document.querySelector('.popup__form-item_input_name');
+const inputAccountName = popUp.querySelector('.popup__form-item_input_name');
 // Input field for desc
-let inputAccountDescription = document.querySelector('.popup__form-item_input_description');
+const inputAccountDescription = popUp.querySelector('.popup__form-item_input_description');
+
+function popUpToggle() {
+  popUp.classList.toggle('popup_opened')
+}
 
 function showPopUp() {
-  // get current account name and description and put in.
+  // get current account name and description and put them into input
   inputAccountName.value = accountName.textContent;
   inputAccountDescription.value = accountDescription.textContent;
   // show popup
-  popUp.classList.add('popup_opened');
-}
-
-function hidePopUp() {
-  popUp.classList.remove('popup_opened');
+  popUpToggle();
 }
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
   accountName.textContent = inputAccountName.value;
   accountDescription.textContent = inputAccountDescription.value;
-  hidePopUp();
+  popUpToggle();
 }
 
 editButton.addEventListener('click', showPopUp);
-closeButton.addEventListener('click', hidePopUp);
+closeButton.addEventListener('click', popUpToggle);
 saveButton.addEventListener('submit', formSubmitHandler);
