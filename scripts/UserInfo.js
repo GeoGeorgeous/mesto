24d8b5fbfa0c90bccb896914b9341e577937d3e3
George Gorchev <georgegorchev@yandex.ru>
2020-10-08 22:45:24 +0300

    feat: add UserInfo.js

---
+++ scripts/UserInfo.js
@@ -0,0 +1,13 @@
+export default class UserInfo {
+  constructor({userData}) {
+    this.userName = userData.userName;
+    this.userDescription = userData.userDescription;
+  }
+  getUserInfo() {
+
+  }
+
+  setUserInfo() {
+    
+  }
+}
